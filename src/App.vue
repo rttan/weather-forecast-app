8657c2739c1c58b3b01fd18ea7071f34456d98f7
Renato Tan <renatotan@Renatos-MacBook-Air.local>
2022-03-12 11:09:28 +0800

    install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld class="bg-red-900"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-red-900 text-3xl">
     VUEJS
   </div>
 </template>
