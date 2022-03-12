1920f89006b23ada667ac06b53d2b3896229e01c
Renato Tan <renatotan@Renatos-MacBook-Air.local>
2022-03-12 15:26:41 +0800

    Create table cell component

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,56 +1,78 @@
 <template>
   <div>
-      <pre>
-          {{ $route.params}}
-      </pre>
-      <!-- main title  -->
-    <div v-html="title" class="text- text-3xl text-center py-10">
-    </div>
-    <!-- search result in table view  -->
-    <!-- header titles using v-for for scalability in future -->
+    <div v-html="title" class="text- text-3xl text-center py-10"></div>
     <div class="flex border-2 border-gray-500 md:mx-10">
-        <div v-for="(item,i) in headers" :key="i" class="md:bg-gray-100 w-full border border-gray-700 md:px-10 px-4">
-           {{ item.title }}
-        </div>
+      <div></div>
     </div>
-
     <!-- data content  -->
     <div class="flex border-2 border-gray-500 md:mx-10">
-        <div v-for="(item,i) in headers" :key="i" class="md:bg-gray-100 w-full border border-gray-700 md:px-10 px-4">
-           {{ item.title }}
-        </div>
+      <TableCell :text="result.dt"></TableCell>
+      <TableCell :text="result.weather[0].main"></TableCell>
+      <TableCell :text="result.main.temp"></TableCell>
+      <TableCell :text="result.weather[0].description"></TableCell>
+      <TableCell :text="result.main.pressure"></TableCell>
+      <TableCell :text="result.main.humidity"></TableCell>
     </div>
   </div>
 </template>
 
 <script>
+import TableCell from "@/components/TableCell";
 export default {
-   data(){
-       return{
-            title:"Search Result",
-            headers:[
-                {
-                    title:"Date (mm/dd/yyyy)"
-                },
-                {
-                    title:"Description"
-                },
-                {
-                    title:"Main"
-                },
-                {
-                    title:"Pressure"
-                },
-                {
-                    title:"Humidity"
-                }
-            ]
-       }
-   },
-
-}
+  data() {
+    return {
+      temp: null,
+      description: null,
+      humidity: null,
+      pressure: null,
+      main: null,
+      title: "Search Result",
+      headers: [
+        {
+          title: "Date (mm/dd/yyyy)",
+          display: "all",
+        },
+        {
+          title: "Temp (F)",
+          display: "all",
+        },
+        {
+          title: "Description",
+          display: "mobile",
+        },
+        {
+          title: "Main",
+          display: "mobile",
+        },
+        {
+          title: "Pressure",
+          display: "mobile",
+        },
+        {
+          title: "Humidity",
+          display: "mobile",
+        },
+      ],
+    };
+  },
+  components: {
+    TableCell,
+  },
+  watch: {
+    "$route.params": {
+      handler(e) {
+        if (e.result) {
+          this.result = e.result;
+          console.log("watch e", e.result.main.temp);
+          this.temp = e.result.main.temp;
+          this.desc = e.result.weather.description;
+          this.main = e.result.weather.main;
+          this.humidity = e.result.main.humidity;
+          this.pressure = e.result.main.pressure;
+        }
+      },
+      immediate: true,
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
---
+++ src/components/TableCell.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full md:px-10 px-4 md:bg-gray-100 border border-gray-700">
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text"],
+};
+</script>
+
