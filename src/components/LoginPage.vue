fd21d04753bff3c8b421ac65ead4f2316571a6f7
rttan <rt@rttan.com>
2022-03-12 17:01:39 +0800

    Configure auth name and github email

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,28 +1,52 @@
 <template>
-    <div>
-        <div class="py-10 flex justify-center text-xl">
-           <div class="md:px-24 w-full">
-                {{ loginTitle }}
-                <div class="flex justify-center py-10">
-                    <button  @click="routeTo" class="hover:bg-blue-500 focus:border focus:border-gray-700 bg-blue-400 px-4 py-2 text-white rounded-full w-full md:w-1/2">
-                        LOGIN
-                    </button>
-                </div>
-           </div>
+  <div>
+    <div class="py-10 flex justify-center text-xl">
+      <div class="md:px-24 w-full">
+        {{ loginTitle }}
+        <div class="flex justify-center py-10">
+          <button
+            @click="login"
+            class="
+              hover:bg-blue-500
+              focus:border focus:border-gray-700
+              bg-blue-400
+              px-4
+              py-2
+              text-white
+              rounded-full
+              w-full
+              md:w-1/2
+            "
+          >
+            LOGIN
+          </button>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            loginTitle:"Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city"
+  data() {
+    return {
+      loginTitle:
+        "Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city",
+    };
+  },
+  watch: {
+    "$auth.user": {
+      handler(u) {
+        if(u){
+          this.$router.replace({name:"Search"})
         }
+      },
+      immediate: true,
     },
-    methods:{
-        routeTo(){
-            this.$router.push("/search")
-        }
-    }
-}
+  },
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+  },
+};
 </script>
